cb4e8af65d879c0e7932e48717183405ca3d1e98
ifoulds <i_foulds@live.com>
2016-01-20 12:17:25 -0600

    Updated Get-CDrive.ps1 in Azure Automation.

--- Runbooks/Get-CDrive.ps1
+++ Runbooks/Get-CDrive.ps1
@@ -1,5 +1,3 @@
 $cred = Get-AutomationPSCredential –Name "AzureAutomation"
-#Add-AzureAccount –Credential $cred
-#Select-AzureSubscription "Visual Studio Ultimate with MSDN"
 
 Get-WmiObject Win32_LogicalDisk -ComputerName "AzureLab-AD01" -Filter "DeviceID='C:'" | Select-Object Size,FreeSpace
